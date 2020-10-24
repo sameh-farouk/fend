0838e6258fac667e9a71c7762464c4dbbf84eb81
Sameh Abouelsaad <samehvirus@gmail.com>
2020-10-24 15:32:20 +0200

    feat: Displaying the score_tag in a human readable format

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,23 +1,32 @@
 function handleSubmit(event) {
     event.preventDefault()
-
+    let score_tag = {
+        'P+': 'strong positive',
+        'P': 'positive',
+        'NEU': 'neutral',
+        'N': 'negative',
+        'N+': 'strong negative',
+        'NONE': 'without sentiment'
+    }
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+    if (Client.checkForUrl(formText)) {
+        console.log("::: Form Submitted :::")
+        fetch('http://localhost:8081/api', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+              },
+            body: JSON.stringify({'formtext': formText})
+        })
+        .then(res => res.json())
+        .then(function(res) {
+            document.getElementById('results').innerHTML = score_tag[res.score_tag]
+        })
+    }
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/api', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-          },
-        body: JSON.stringify({'formtext': formText})
-    })
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.score_tag
-    })
+    
 }
 
 export { handleSubmit }
